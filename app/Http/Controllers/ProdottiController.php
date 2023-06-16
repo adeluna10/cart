0c9a95430af3b9c278c561f1879ad7f8002f267b
Andrea De Luna <andrea.deluna@innoteam.it>
2023-06-16 14:50:47 +0200

    esegue e rimuove warning phpstan

--- app/Http/Controllers/ProdottiController.php
+++ app/Http/Controllers/ProdottiController.php
@@ -3,27 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Prodotto;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class ProdottiController extends Controller
 {
-    public function list()
+    /** @return Collection<int, Prodotto> */
+    public function list(): Collection
     {
         return Prodotto::all();
     }
 
-    public function get(Prodotto $prodotto)
+    public function get(Prodotto $prodotto): Prodotto
     {
         return $prodotto;
     }
 
-    public function crea()
+    public function crea(): View
     {
         return view('prodotti.crea');
     }
 
-    public function salva(Request $request)
+    public function salva(Request $request): RedirectResponse
     {
         $request->validate([
             'nome' => ['required'],
